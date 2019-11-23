bfafcb93715feae680e71261456e1ce950151573
Anton Gorev <kvirund@gmail.com>
2019-11-23 03:54:07 -0800

    Implemented simple button with counter and attempted to add contractions chart.

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+import CanvasJSReact from './lib/canvasjs.react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ContractionButton = (props) => {
+    const [started, setStarted] = useState(false);
+    const [variant, setVariant] = useState("primary");
+    const [text, setText] = useState("Start");
+    const [timer, setTimer] = useState(null);
+    const [startTime, setStartTime] = useState(0);
+
+    const checkpoint = (started) => {
+        setText("Stop (elapsed " + getElapsedSeconds(started) + " seconds)");
+
+        props.onContractionTick(getElapsedSeconds(started));
+    }
+
+    const getElapsedSeconds = (startTime) => {
+        return Math.round((Date.now() - startTime)/1000);
+    }
+
+    const onClick = () => {
+        if (!started) {
+            setStarted(true);
+            setVariant("success");
+            setText("Stop");
+
+            var now = Date.now();
+            setStartTime(now);
+            setTimer(setInterval(() => checkpoint(now), 1000));
+        } else {
+            clearInterval(timer);
+
+            setStarted(false);
+            setVariant("primary");
+            setText("Start");
+
+            props.onNewContraction(getElapsedSeconds(startTime));
+        }
+    }
+
+    return (
+        <Button variant={variant} onClick={onClick}>{text}</Button>
+        )
+}
+
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const options = {
+      title: {
+        text: "Basic Column Chart in React"
+      },
+      data: [{
+                type: "column",
+                dataPoints: []
+       }]
+   }
+
+const App = () => {
+    const [chart, setChart] = useState(null);
+
+    var currentColumn = null;
+
+    const onNewContraction = (length) => {
+        if (null == currentColumn) {
+            currentColumn = {label: Date(Date.now()).toString(), y: length};
+            chart.data[0].dataPoints.push(currentColumn);
+        } else {
+            currentColumn.y = length;
+        }
+
+        chart.render();
+        currentColumn = null;
+    }
+
+    const onContractionTick = (length) => {
+        if (null == currentColumn) {
+            currentColumn = {label: Date(Date.now()).toString(), y: length};
+            chart.data[0].dataPoints.push(currentColumn);
+        } else {
+            currentColumn.y = length;
+        }
+
+        chart.render();
+    }
+
+    return (
+        <Container className="p-3">
+            <div className="container-fluid">
+                <CanvasJSChart options={options} onRef={ref => { setChart(ref); console.log(ref); } } />
+                <ContractionButton onNewContraction={onNewContraction} onContractionTick={onContractionTick} />
+            </div>
+        </Container>
+    );
 }
 
 export default App;
